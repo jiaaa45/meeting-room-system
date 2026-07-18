137722e1cd56c437262fdc065df19a2a5582fb08
jiaaa45 <chaying60@gmail.com>
2026-07-19 00:40:16 +0800

    chore: initialize Spring Boot project skeleton

---
+++ src/main/java/com/company/meetingroom/MeetingRoomSystemApplication.java
@@ -0,0 +1,13 @@
+package com.company.meetingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/meeting_room_system/MeetingRoomSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.meeting_room_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
